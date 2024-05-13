ed980d7abb407d9e0bf64d42fec8f3f79b66da02
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-05-13 22:35:18 +0530

    Update main.tf

--- vault/main.tf
+++ vault/main.tf
@@ -27,6 +27,6 @@ resource "aws_instance" "my_instance" {
 
   tags = {
     Name = "test"
-    Secret = data.vault_kv_secret_v2.example.data["foo"]
+    Secret = data.vault_kv_secret_v2.example.data["user.name"]
   }
 }
